c7187cf2f04254dc07f5486bf2b1906c202b880c
KhadijaHasnain <113336160+KhadijaHasnain@users.noreply.github.com>
2023-07-06 20:29:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".taxt", {
+    strings: ["software Engineer", "Web Development", "Laravel PHP Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
